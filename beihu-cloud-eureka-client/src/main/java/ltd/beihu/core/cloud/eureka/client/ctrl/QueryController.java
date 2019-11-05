ba0b4b1fb196c33f838a2c1351633ef266be6866
zjz6b6 <407091038@qq.com>
2019-11-05 17:38:28 +0800

    添加feign测试

--- beihu-cloud-eureka-client/src/main/java/ltd/beihu/core/cloud/eureka/client/ctrl/QueryController.java
+++ beihu-cloud-eureka-client/src/main/java/ltd/beihu/core/cloud/eureka/client/ctrl/QueryController.java
@@ -1,9 +1,11 @@
 package ltd.beihu.core.cloud.eureka.client.ctrl;
 
+import ltd.beihu.core.cloud.eureka.client.service.FeignTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class QueryController {
 
     @Autowired
-    EurekaClientConfigBean eurekaClientConfigBean;
+    private EurekaClientConfigBean eurekaClientConfigBean;
+    @Autowired
+    private FeignTestService feignTestService;
+
+    @GetMapping(value = "/search/github")
+    public String searchGithubRepoByStr(@RequestParam("str") String queryStr) {
+        return feignTestService.searchRepo(queryStr);
+    }
 
     @GetMapping("/eureka-server")
     public Object getEurekaServerUrl(){
         return eurekaClientConfigBean.getServiceUrl();
     }
+
+
 }
