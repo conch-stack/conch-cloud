7c54bb6ef2915372e0dd53ce9a35d33fc1b24b66
zjz6b6 <407091038@qq.com>
2019-11-06 00:44:43 +0800

    feign添加fileupload支持

--- beihu-cloud-eureka-client/src/main/java/ltd/beihu/core/cloud/eureka/client/ctrl/QueryController.java
+++ beihu-cloud-eureka-client/src/main/java/ltd/beihu/core/cloud/eureka/client/ctrl/QueryController.java
@@ -1,12 +1,12 @@
 package ltd.beihu.core.cloud.eureka.client.ctrl;
 
 import ltd.beihu.core.cloud.eureka.client.service.FeignTestService;
+import ltd.beihu.core.cloud.eureka.client.service.FileUploadFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/query")
@@ -16,9 +16,11 @@ public class QueryController {
     private EurekaClientConfigBean eurekaClientConfigBean;
     @Autowired
     private FeignTestService feignTestService;
+    @Autowired
+    private FileUploadFeignService fileUploadFeignService;
 
     /**
-     * 测试Feign
+     * 测试Feign简单请求
      * http://localhost:8081/query/search/github?str=spring-cloud-dubbo
      */
     @GetMapping(value = "/search/github")
@@ -34,5 +36,12 @@ public class QueryController {
         return eurekaClientConfigBean.getServiceUrl();
     }
 
+    /**
+     * 测试Feign文件上传
+     */
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String imageUpload(MultipartFile file) throws Exception{
+        return fileUploadFeignService.fileUpload(file);
+    }
 
 }
---
+++ beihu-cloud-provider/src/test/java/ltd/beihu/core/cloud/provider/BeihuCloudProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.beihu.core.cloud.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeihuCloudProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
