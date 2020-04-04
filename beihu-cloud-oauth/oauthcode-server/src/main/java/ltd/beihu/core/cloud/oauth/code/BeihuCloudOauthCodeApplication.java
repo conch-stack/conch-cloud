a8f565e82a88571789ef70127cb150dd37c24c6e
zjz6b6 <407091038@qq.com>
2020-04-05 00:18:26 +0800

    add OAuth2 authorization_code demo

---
+++ beihu-cloud-oauth/oauthcode-server/src/main/java/ltd/beihu/core/cloud/oauth/code/BeihuCloudOauthCodeApplication.java
@@ -0,0 +1,13 @@
+package ltd.beihu.core.cloud.oauth.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeihuCloudOauthCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeihuCloudOauthCodeApplication.class, args);
+    }
+
+}
---
+++ beihu-cloud-oauth/oauthcode-server/src/main/java/ltd/beihu/core/cloud/oauth/code/config/Oauth2AuthorizationServer.java
@@ -0,0 +1,28 @@
+package ltd.beihu.core.cloud.oauth.code.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * 授权服务器
+ *
+ * @author Adam
+ * @date 2020/4/4
+ */
+@Configuration
+@EnableAuthorizationServer
+public class Oauth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client-id")
+                .secret("112233")
+                .redirectUris("http://localhost:9001/callback")
+                // 授权码模式
+                .authorizedGrantTypes("authorization_code")
+                .scopes("read_userinfo");
+    }
+}
